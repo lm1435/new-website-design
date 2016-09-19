b622b14cfa97b25b5f1379147f72c54d5c256501
Luis Meza <Meza@Luiss-MacBook-Pro.local>
2016-09-18 23:43:08 -0500

    website layout and half design

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	//smooth scrolling
+	var $root = $('html, body');
+  $('.navbar-nav a').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+      scrollTop: $(href).offset().top
+    }, 500, function () {
+      window.location.hash = href;
+    });
+    return false;
+  });
+
+});
