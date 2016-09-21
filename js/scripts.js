6a742eb7f356790a5e4bee34f21d7110c552ccdd
Luis Meza <Meza@Luiss-MacBook-Pro.local>
2016-09-20 23:59:06 -0500

    side panel nav css for mobile first

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	//smooth scrolling
+	var $root = $('html, body');
+  $('.navbar-nav a').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+      scrollTop: $(href).offset().top
+    }, 500, function () {
+      window.location.hash = href;
+    });
+    return false;
+  });
+  
+});
+ 
