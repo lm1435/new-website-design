4690e6ef8c8facaf76240a99b6930d6bda680715
Luis Meza <luismeza01@gmail.com>
2016-10-03 22:51:18 -0500

    formatting and fixing formspree page

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,39 @@
 $(document).ready(function(){
 
+  // cycling through images array //
+
+  var imgArray = new Array();
+
+  imgArray[0] = new Image();
+  imgArray[0].src = 'img/html5.png';
+
+  imgArray[1] = new Image();
+  imgArray[1].src = 'img/css.png';
+
+  imgArray[2] = new Image();
+  imgArray[2].src = 'img/js.png';
+
+  imgArray[3] = new Image();
+  imgArray[3].src = 'img/jquery.jpg';
+
+  imgArray[4] = new Image();
+  imgArray[4].src = 'img/bootstrap.png';
+
+  imgArray[5] = new Image();
+  imgArray[5].src = 'img/rails.png';
+
+  function displayImg(arr){
+      $('.images').html(arr[0]);
+      var i = 1;
+      setInterval(
+          function(){
+              $('.images').html(arr[i]);
+              i++;
+              if(i >= imgArray.length) i = 0;
+          },1500);
+  }
+
+  displayImg(imgArray);
 //smooth scrolling//
 
   var $root = $('html, body');
@@ -11,45 +45,17 @@ $(document).ready(function(){
     window.location.hash = href;
   });
   return false;
+
+
+
 });
 
 // formspree protection from spam //
 
 var contactform = document.getElementById('contactform');
-  contactform.setAttribute('action', '//formspree.io/' + 'luis' + 'meza01' + '@' + 'gmail' + '.' + 'com');
+  contactform.setAttribute('action', 'https://formspree.io/' + 'luis' + 'meza01' + '@' + 'gmail' + '.' + 'com');
 });
 
-// cycling through images array //
-
-var imgArray = new Array();
-
-imgArray[0] = new Image();
-imgArray[0].src = 'img/html5.png';
-
-imgArray[1] = new Image();
-imgArray[1].src = 'img/css.png';
-
-imgArray[2] = new Image();
-imgArray[2].src = 'img/js.png';
-
-imgArray[3] = new Image();
-imgArray[3].src = 'img/jquery.jpg';
-
-imgArray[4] = new Image();
-imgArray[4].src = 'img/rails.png';
-
-function displayImg(arr){
-    $('.images').html(arr[0]);
-    var i = 1;
-    setInterval(
-        function(){
-            $('.images').html(arr[i]);
-            i++;
-            if(i >= imgArray.length) i = 0;
-        },1500);
-}
-
-displayImg(imgArray);
 
 // Map API //
 
@@ -57,7 +63,7 @@ var map;
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 30.257927, lng: -97.742520},
-    zoom: 15,
+    zoom: 14,
     mapTypeControl: false,
     disableDoubleClickZoom: true,
     scrollwheel: false,
