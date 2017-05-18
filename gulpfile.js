207b31e4d8de84b6c300969890a1afd2e8e703f5
Luis Meza <luismeza01@gmail.com>
2017-05-18 18:17:43 -0500

    added SEO features

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var cssmin = require('gulp-cssmin');
+var sitemap = require('gulp-sitemap');
+
+gulp.task('sitemap', function() {
+  gulp.src('*.html', {
+    read: false
+  })
+  .pipe(sitemap({
+    siteUrl:'http://luismeza.io'
+  }))
+  .pipe(gulp.dest("./"));
+});
 
 gulp.task('default', function(){
   gulp.src('src/**/*.css')
